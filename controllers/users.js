b0ad81ac168fc036b13a46d52d01b09cfb77657c
Fernando Espana <fernando.espana@globant.com>
2022-12-14 22:32:23 -0500

    Install exppress-validator library,create middle ware to validate required fields.

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const getUsers = async (req, res) => {
 	const users = await User.find({}, 'name email role');
@@ -8,17 +10,33 @@ const getUsers = async (req, res) => {
 	});
 };
 
-const createUser = async (req, res) => {
+const createUser = async (req, res = response) => {
 	const { name, password, email } = req.body;
 
-	var user = new User(req.body);
+	try {
+		const emailAlreadyExits = await User.findOne({ email });
 
-	await user.save();
+		if (emailAlreadyExits) {
+			return res.status(400).json({
+				ok: false,
+				mas: 'Email already exists',
+			});
+		}
 
-	res.json({
-		ok: true,
-		user,
-	});
+		var user = new User(req.body);
+		await user.save();
+
+		res.json({
+			ok: true,
+			user,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Unespected Error',
+		});
+	}
 };
 
 module.exports = {
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,25 @@
-/* 
+/*
   Route /api/users
 */
-
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { getUsers, createUser } = require('../controllers/users');
+const { validateFields } = require('../middlewares/field-validator');
 
 router = Router();
 
 router.get('/', getUsers);
 
-router.post('/', createUser);
+router.post(
+	'/',
+	// Middlewares to validate several required fields
+	[
+		check('name', 'The name is mandatory').not().isEmpty(),
+		check('password', 'The password is mandatory').not().isEmpty(),
+		check('email', 'Email wrong format').isEmail(),
+		validateFields,
+	],
+	createUser
+);
 
 module.exports = router;
